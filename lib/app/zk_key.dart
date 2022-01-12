4cba07e215a1a2fa015a831e8b917e965d516131
1328148451@qq.com <1328148451@qq.com>
2022-01-12 14:01:53 +0800

    first

--- lib/app/zk_key.dart
+++ lib/app/zk_key.dart
@@ -3,5 +3,6 @@ import 'package:flutter/material.dart';
 class ZkValueKey extends ValueKey<String> {
   static const keyMainRoute = ZkValueKey("zk_main_route");
   static const keyHomeAppbar = ZkValueKey("zk_home_appbar");
+  static const keyLoginAppbar = ZkValueKey("zk_login_appbar");
   const ZkValueKey(String s) : super(s);
 }
