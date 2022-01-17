7c7b3337ce0cb82702bd430ca2bdb1eca2b4ee9b
wuzhikun <shengdawuzk@163.com>
2022-01-17 13:21:05 +0800

    add utils.core

---
+++ lib/utils/zk_fly_core.dart
@@ -0,0 +1,23 @@
+import 'dart:async';
+import 'dart:ui' as ui;
+
+import 'package:flutter/services.dart';
+
+class ZkFlyCore {
+  static Future<ui.Image?> getAssetImage(String asset,
+      {int? width, int? height}) async {
+    ByteData data = await rootBundle.load(asset);
+    ui.Codec codec = await ui.instantiateImageCodec(data.buffer.asUint8List(),
+        targetWidth: width, targetHeight: height);
+    ui.FrameInfo fi = await codec.getNextFrame();
+    return fi.image;
+  }
+
+  Future<ui.Image?> getNetImage(String url, {width, height}) async {
+    ByteData data = await NetworkAssetBundle(Uri.parse(url)).load(url);
+    ui.Codec codec = await ui.instantiateImageCodec(data.buffer.asUint8List(),
+        targetWidth: width, targetHeight: height);
+    ui.FrameInfo fi = await codec.getNextFrame();
+    return fi.image;
+  }
+}
